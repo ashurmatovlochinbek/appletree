f0f4489cc371db307ab73b14a5cc1c93d9beafb9
Ashurmatov Lochinbek <l10.ashurmatov@gmail.com>
2023-06-29 15:10:31 +0500

    created a writeJSON helper method

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -12,13 +11,14 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"environment": app.config.env,
 		"version": version,
 	}
-	//Convert our map into a JSON object
-	js, err := json.Marshal(data)
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+
 }
 
