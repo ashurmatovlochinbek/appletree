2f9019bdce23cb6661389ad09489306677fcc2f3
Ashurmatov Lochinbek <l10.ashurmatov@gmail.com>
2023-06-29 12:37:18 +0500

    moved the code that chcks for id parameter into its own method. Methods name is readIDParam

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // createSchoolHandler for the "POST /v1/schools endpoint"
@@ -16,14 +13,10 @@ func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Reque
 
 // createSchoolHandler for the "GET /v1/schools/:id endpoint"
 func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
-	// Use the "ParamsFromContext() function to get the request context as a sclice"
-	params := httprouter.ParamsFromContext(r.Context())
-	//Get the value of the "id" parameter 
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
-
 	fmt.Fprintf(w, "show the details for school %d\n", id)
 }
